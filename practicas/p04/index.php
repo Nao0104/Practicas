c841c6bdc8d82f6645d11956e41c49fb3bcb0b61
Nao0104 <ixchelnaomicortesmontano01@gmail.com>
2023-02-09 14:50:56 -0600

    Ejercicio5 P04

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 3</title>
+    <title>Práctica 4</title>
     <style type="text/css">
       ol { 
       list-style-type: none;
@@ -14,6 +14,7 @@
 <body>
     <?php
     require_once __DIR__.'/src/funciones.php';
+    /*require_once __DIR__.'/src/script1.php';*/
     ?>
     <div>
         <h3>Ejercicio 1</h3>
@@ -130,23 +131,44 @@
             ?>
         </p>
     </div>
+    <hr>
     <div>
         <h3>Ejercicio 5</h3>
-        <p>Crear un arreglo cuyos <strong>índices</strong> van de 97 a 122 y cuyos <strong>valores</strong> son las letras de la 'a' a la 'z'. Usa la función <strong>chr(n)</strong> que devuelve el caracter cuyo código ASCII es <strong>n</strong> para poner el valor en cada índice. Es decir:</p>
         <p>
-            [97] => a <br>
-            [98] => b <br>
-            [99] => c <br>
-            … <br>
-            [122] => z
+            Usar las variables $edad y $sexo en una instrucción if para identificar una persona de sexo "femenino", 
+            cuya edad oscile entre los 18 y 35 años y mostrar un mensaje de bienvenida apropiado. Por ejemplo:
         </p>
         <p>
-            R:
-            <?php
-            // AQUÍ VA EL CÓDIGO DE SOLUCIÓN
-            ?>
+            <em>Bienvenida, usted está en el rango de edad permitido.</em>
+        </p>
+        <p>
+            En caso contrario, deberá devolverse otro mensaje indicando el error.
+        </p>
+        <ul>
+            <li>Los valores para $edad y $sexo se deben obtener por medio de un formulario en HTML.</li>
+            <li>Utilizar el la Variable Superglobal $_POST (revisar documentación).</li>
+        </ul>
+        </p>
+        <p>
+        <b> R:</b>
+            <form id="formulario1" action="./src/script1.php" method="post">
+            <fieldset>
+                <legend>Información Personal</legend>
+                <ol>
+                <li><label>Edad:</label> <input type="text" name="edad"></li>
+                <li><label>Sexo:</label> <select name="sexo">
+                    <option >Masculino</option>
+                    <option >Femenino</option>
+                </select><br></li>
+                </ol>
+            </fieldset>
+            <p>
+                <input type="submit" value="Enviar">
+            </p>
+            </form>
         </p>
     </div>
+    <hr>
     <div>
         <h3>Ejercicio 6</h3>
         <p>
