94d0316f96c239f005c8824d60be951ec444a24b
Nao0104 <ixchelnaomicortesmontano01@gmail.com>
2023-03-23 23:31:49 -0600

    Commit p08 terminado

---
+++ practicas/p08/product_app/backend/create.php
@@ -0,0 +1,29 @@
+<?php
+    include_once __DIR__.'/database.php';
+
+    // SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
+    $producto = file_get_contents('php://input');
+    if(!empty($producto)) {
+        // SE TRANSFORMA EL STRING DEL JASON A OBJETO
+        $jsonOBJ = json_decode($producto, true);
+
+        $nombreobj = $jsonOBJ["nombre"];
+        $marcaobj = $jsonOBJ["marca"];
+        $modeloobj = $jsonOBJ["modelo"];
+        $precioobj = $jsonOBJ["precio"];
+        $detallesobj = $jsonOBJ["detalles"];
+        $unidadesobj = $jsonOBJ["unidades"];
+        $imagenobj = $jsonOBJ["imagen"];
+
+        $consultaObjs = $conexion->query("SELECT * FROM productos WHERE nombre='{$nombreobj}' AND eliminado=0");
+        if ($consultaObjs->num_rows != 0) {
+            echo "ESTE PRODUCTO YA EXISTE";   
+        }
+        else {  if ( $result = $conexion->query("INSERT INTO productos VALUES (NULL, '{$nombreobj}', '{$marcaobj}', '{$modeloobj}', '{$precioobj}', '{$detallesobj}', '{$unidadesobj}', '{$imagenobj}', '0')") ) {
+                echo "ESTE PRODUCTO FUE  INSERTADO CORRECTAMENTE ";   }
+        else { die('Query Error: '.mysqli_error($conexion));
+                echo "ESTE PRODUCTO NO  FUE INSERTADO";             }
+        } $conexion->close();
+        //echo '[SERVIDOR] Nombre: '.$jsonOBJ->nombre;
+    }
+?>
